e1297b59d4b5d110a777da805668b7dcb7507e5b
TzeYangLim <limty-wm19@student.tarc.edu.my>
2022-08-25 16:46:03 +0800

    test

--- admin/new-appointment.php
+++ admin/new-appointment.php
@@ -131,10 +131,6 @@ include('includes/dbconnection.php');
 				}
 			}
 		</script>
-		<!--scrolling js-->
-		<script src="js/jquery.nicescroll.js"></script>
-		<script src="js/scripts.js"></script>
-		<!--//scrolling js-->
 		<!-- Bootstrap Core JavaScript -->
 		<script src="js/bootstrap.js"> </script>
 	</body>
