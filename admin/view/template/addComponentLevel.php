8c133a149448681d0296207cf728132f954a2a13
Nei Jun Xian <neijunxian@gmail.com>
2022-08-26 23:24:32 +0800

    test css

--- admin/view/template/addComponentLevel.php
+++ admin/view/template/addComponentLevel.php
@@ -88,8 +88,8 @@ include('includes/db_connection.php');
                                                                                         }  ?> </p>
 
 
-                                <div class="form-group"> <label for="exampleInputEmail1">Component Name</label> <input type="text" class="form-control" id="sername" name="sername" placeholder="Component Name" value="" required="true"> </div>
-                                <div class="form-group"> <label for="exampleInputPassword1">Component Level</label> <input type="text" id="cost" name="cost" class="form-control" placeholder="Component Level" value="" required="true"> </div>
+                                <div class="form-group"> <label for="exampleInputEmail1">Component Name</label> <input type="text" class="form-control" id="cmpname" name="cmpname" placeholder="Component Name" value="" required="true"> </div>
+                                <div class="form-group"> <label for="exampleInputPassword1">Component Level</label> <input type="text" id="cmplv" name="cmplv" class="form-control" placeholder="Component Level" value="" required="true"> </div>
 
                                 <button type="submit" name="submit" class="btn btn-default">Add</button>
                             </form>
