26f9275181d02b024f8251f88fc76657466a907f
Nei Jun Xian <neijunxian@gmail.com>
2022-08-27 00:03:40 +0800

    update boostrap version

--- admin/view/template/addComponentLevel.php
+++ admin/view/template/addComponentLevel.php
@@ -91,7 +91,7 @@ include('includes/db_connection.php');
                                 <div class="form-group"> <label for="exampleInputEmail1">Component Name</label> <input type="text" class="form-control" id="cmpname" name="cmpname" placeholder="Component Name" value="" required="true"> </div>
                                 <div class="form-group"> <label for="exampleInputPassword1">Component Level</label> <input type="text" id="cmplv" name="cmplv" class="form-control" placeholder="Component Level" value="" required="true"> </div>
 
-                                <button type="submit" name="submit" class="btn btn-default">Add</button>
+                                <button type="submit" name="submit" class="btn btn-default ">Add</button>
                             </form>
                         </div>
 
