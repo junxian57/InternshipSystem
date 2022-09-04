5ef8de653b235eb287dfaa2ebb4ed2cc64d3eb76
Bryson Tai <brysontai10@gmail.com>
2022-09-05 00:31:14 +0800

    Update Company

--- Client/companyRegistration.php
+++ Client/companyRegistration.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+
+    <link rel="stylesheet" href="../Client/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="../Client/css/animation.css">
+
+    <link rel="stylesheet" href="../Client/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../Client/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../Client/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="../Client/css/aos.css">
+
+    <link rel="stylesheet" href="../Client/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="../Client/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../Client/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="../Client/css/flaticon.css">
+    <link rel="stylesheet" href="../Client/css/icomoon.css">
+    <link rel="stylesheet" href="../Client/css/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        echo "Hello 123"
+    include 'includes/header.php';
+    ?>
+
+    <?php
+    include 'includes/footer.php';
     ?>
 </body>
+
 </html>
