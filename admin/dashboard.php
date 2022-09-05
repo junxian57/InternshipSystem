afbb737344bb8c5d9aeacc498dfba7a0a7abf564
XiaoTong <wongxt-wm19@student.tarc.edu.my>
2022-09-05 13:30:41 +0800

    bug

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,16 +1,32 @@
 <?php
 session_start();
 error_reporting(0);
-/*include('includes/dbconnection.php');
-if (strlen($_SESSION['bpmsaid']==0)) {
-  header('location:logout.php');
-  } */
+include('includes/dbconnection.php');
+/*if (strlen($_SESSION['bpmsaid'] == 0)) {
+	header('location:logout.php');
+} else {
+
+	if (isset($_POST['submit'])) {
+		$sername = $_POST['sername'];
+		$cost = $_POST['cost'];
+
+
+
+		$query = mysqli_query($con, "insert into  tblservices(ServiceName,Cost) value('$sername','$cost')");
+		if ($query) {
+			echo "<script>alert('Service has been added.');</script>";
+			echo "<script>window.location.href = 'add-services.php'</script>";
+			$msg = "";
+		} else {
+			echo "<script>alert('Something Went Wrong. Please try again.');</script>";
+		}
+	}*/
 ?>
 <!DOCTYPE HTML>
 <html>
 
 <head>
-	<title>BPMS | Admin Dashboard</title>
+	<title>ITP System | Dashboard</title>
 
 	<script type="application/x-javascript">
 		addEventListener("load", function() {
@@ -42,238 +58,56 @@ if (strlen($_SESSION['bpmsaid']==0)) {
 		new WOW().init();
 	</script>
 	<!--//end-animate-->
-	<!-- chart -->
-	<script src="js/Chart.js"></script>
-	<!-- //chart -->
-	<!--Calender-->
-	<link rel="stylesheet" href="css/clndr.css" type="text/css" />
-	<script src="js/underscore-min.js" type="text/javascript"></script>
-	<script src="js/moment-2.2.1.js" type="text/javascript"></script>
-	<script src="js/clndr.js" type="text/javascript"></script>
-	<script src="js/site.js" type="text/javascript"></script>
-	<!--End Calender-->
 	<!-- Metis Menu -->
 	<script src="js/metisMenu.min.js"></script>
 	<script src="js/custom.js"></script>
 	<link href="css/custom.css" rel="stylesheet">
 	<!--//Metis Menu -->
-	<!--Notification-->
-	<link href="css/toastr.min.css" rel="stylesheet" type="text/css" media="all">
-	<script src="js/toastr.min.js"></script>
-	<!--//Notification-->
 </head>
 
 <body class="cbp-spmenu-push">
 	<div class="main-content">
-
+		<!--left-fixed -navigation-->
 		<?php include_once('includes/sidebar.php'); ?>
-
+		<!--left-fixed -navigation-->
+		<!-- header-starts -->
 		<?php include_once('includes/header.php'); ?>
+		<!-- //header-ends -->
 		<!-- main content start-->
-		<div id="page-wrapper" class="row calender widget-shadow">
+		<div id="page-wrapper">
 			<div class="main-page">
-
-
-				<div class="row calender widget-shadow">
-					<div class="row-one">
-						<div class="col-md-4 widget">
-							<?php $query1 = mysqli_query($con, "Select * from tblcustomers");
-							$totalcust = mysqli_num_rows($query1);
-							?>
-							<div class="stats-left ">
-								<h5>123</h5>
-								<h4>Customer</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $totalcust; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="col-md-4 widget states-mdl">
-							<?php $query2 = mysqli_query($con, "Select * from tblappointment");
-							$totalappointment = mysqli_num_rows($query2);
-							?>
-							<div class="stats-left">
-								<h5>Total</h5>
-								<h4>Appointment</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $totalappointment; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="col-md-4 widget states-last">
-							<?php $query3 = mysqli_query($con, "Select * from tblappointment where Status='1'");
-							$totalaccapt = mysqli_num_rows($query3);
-							?>
-							<div class="stats-left">
-								<h5>Total</h5>
-								<h4>Accepted Apt</h4>
-							</div>
-							<div class="stats-right">
-								<label><?php echo $totalaccapt; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-
-				</div>
-
-				<div class="row calender widget-shadow">
-					<div class="row-one">
-						<div class="col-md-4 widget">
-							<?php $query4 = mysqli_query($con, "Select * from tblappointment where Status='1'");
-							$totalrejapt = mysqli_num_rows($query4);
-							?>
-							<div class="stats-left ">
-								<h5>Total</h5>
-								<h4>Rejected Apt</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $totalrejapt; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="col-md-4 widget states-mdl">
-							<?php $query5 = mysqli_query($con, "Select * from  tblservices");
-							$totalser = mysqli_num_rows($query5);
-							?>
-							<div class="stats-left">
-								<h5>Total</h5>
-								<h4>Services</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $totalser; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="col-md-4 widget states-last">
-							<?php
-							//todays sale
-							$query6 = mysqli_query($con, "select tblinvoice.ServiceId as ServiceId, tblservices.Cost
- from tblinvoice 
-  join tblservices  on tblservices.ID=tblinvoice.ServiceId where date(PostingDate)=CURDATE();");
-							while ($row = mysqli_fetch_array($query6)) {
-								$todays_sale = $row['Cost'];
-								$todysale += $todays_sale;
-							}
-							?>
-							<div class="stats-left">
-								<h5>Today</h5>
-								<h4>Sales</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $todysale; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-
-				</div>
-
-				<div class="row calender widget-shadow">
-					<div class="row-one">
-						<div class="col-md-4 widget">
-							<?php
-							//Yesterday's sale
-							$query7 = mysqli_query($con, "select tblinvoice.ServiceId as ServiceId, tblservices.Cost
- from tblinvoice 
-  join tblservices  on tblservices.ID=tblinvoice.ServiceId where date(PostingDate)=CURDATE()-1;");
-							while ($row7 = mysqli_fetch_array($query7)) {
-								$yesterdays_sale = $row7['Cost'];
-								$yesterdaysale += $yesterdays_sale;
-							}
-							?>
-							<div class="stats-left ">
-								<h5>Yesterday</h5>
-								<h4>Sales</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $yesterdaysale; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="col-md-4 widget states-mdl">
-							<?php
-							//Last Sevendays Sale
-							$query8 = mysqli_query($con, "select tblinvoice.ServiceId as ServiceId, tblservices.Cost
- from tblinvoice 
-  join tblservices  on tblservices.ID=tblinvoice.ServiceId where date(PostingDate)>=(DATE(NOW()) - INTERVAL 7 DAY);");
-							while ($row8 = mysqli_fetch_array($query8)) {
-								$sevendays_sale = $row8['Cost'];
-								$tseven += $sevendays_sale;
-							}
-							?>
-							<div class="stats-left">
-								<h5>Last Sevendays</h5>
-								<h4>Sale</h4>
-							</div>
-							<div class="stats-right">
-								<label> <?php echo $tseven; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="col-md-4 widget states-last">
-							<?php
-							//Total Sale
-							$query9 = mysqli_query($con, "select tblinvoice.ServiceId as ServiceId, tblservices.Cost
- from tblinvoice 
-  join tblservices  on tblservices.ID=tblinvoice.ServiceId");
-							while ($row9 = mysqli_fetch_array($query9)) {
-								$total_sale = $row9['Cost'];
-								$totalsale += $total_sale;
-							}
-							?>
-							<div class="stats-left">
-								<h5>Total</h5>
-								<h4>Sales</h4>
-							</div>
-							<div class="stats-right">
-								<label><?php echo $totalsale; ?></label>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-
-				</div>
+				
 			</div>
-			<div class="clearfix"> </div>
 		</div>
-	</div>
-	<!--footer-->
-	<?php include_once('includes/footer.php'); ?>
-	<!--//footer-->
-	</div>
-	<!-- Classie -->
-	<script src="js/classie.js"></script>
-	<script>
-		var menuLeft = document.getElementById('cbp-spmenu-s1'),
-			showLeftPush = document.getElementById('showLeftPush'),
-			body = document.body;
+		<footer><?php include_once('includes/footer.php'); ?></footer>
+		<!-- Classie -->
+		<script src="js/classie.js"></script>
+		<script>
+			var menuLeft = document.getElementById('cbp-spmenu-s1'),
+				showLeftPush = document.getElementById('showLeftPush'),
+				body = document.body;
 
-		showLeftPush.onclick = function() {
-			classie.toggle(this, 'active');
-			classie.toggle(body, 'cbp-spmenu-push-toright');
-			classie.toggle(menuLeft, 'cbp-spmenu-open');
-			disableOther('showLeftPush');
-		};
+			showLeftPush.onclick = function() {
+				classie.toggle(this, 'active');
+				classie.toggle(body, 'cbp-spmenu-push-toright');
+				classie.toggle(menuLeft, 'cbp-spmenu-open');
+				disableOther('showLeftPush');
+			};
 
-
-		function disableOther(button) {
-			if (button !== 'showLeftPush') {
-				classie.toggle(showLeftPush, 'disabled');
+			function disableOther(button) {
+				if (button !== 'showLeftPush') {
+					classie.toggle(showLeftPush, 'disabled');
+				}
 			}
-		}
-	</script>
-	<!--scrolling js-->
-	<script src="js/jquery.nicescroll.js"></script>
-	<script src="js/scripts.js"></script>
-	<!--//scrolling js-->
-	<!-- Bootstrap Core JavaScript -->
-	<script src="js/bootstrap.js"> </script>
+		</script>
+		<!--scrolling js-->
+		<script src="js/jquery.nicescroll.js"></script>
+		<script src="js/scripts.js"></script>
+		<!--//scrolling js-->
+		<!-- Bootstrap Core JavaScript -->
+		<script src="js/bootstrap.js"> </script>
 </body>
 
 </html>
+<?php //} 
+?>
