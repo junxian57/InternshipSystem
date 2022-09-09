6bb954a126e2d2898f58613a1db7ef78fe2815c0
Wei Kang Yi <weiky-pm19@student.tarc.edu.my>
2022-09-09 21:56:33 +0800

    StudentRegister

---
+++ Client/view/page/ky-StudentRegister.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/db_connection.php');
+/*if (strlen($_SESSION['bpmsaid'] == 0)) {
+	header('location:logout.php');
+} else {
+
+	if (isset($_POST['submit'])) {
+		$sername = $_POST['sername'];
+		$cost = $_POST['cost'];
+
+
+
+		$query = mysqli_query($con, "insert into  tblservices(ServiceName,Cost) value('$sername','$cost')");
+		if ($query) {
+			echo "<script>alert('Service has been added.');</script>";
+			echo "<script>window.location.href = 'add-services.php'</script>";
+			$msg = "";
+		} else {
+			echo "<script>alert('Something Went Wrong. Please try again.');</script>";
+		}
+	}*/
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>ITP SYSTEM</title>
+    <script type="application/x-javascript">
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <link href="../../css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <link href="../../css/style.css" rel='stylesheet' type='text/css' />
+    <link href="../../css/font-awesome.css" rel="stylesheet">
+    <script src="../../js/jquery-1.11.1.min.js"></script>
+    <script src="../../js/modernizr.custom.js"></script>
+    <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href="../../css/animate.css" rel="stylesheet" type="text/css" media="all">
+    <script src="../../js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <script src="../../js/metisMenu.min.js"></script>
+    <script src="../../js/custom.js"></script>
+    <link href="../../css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../scss/studentSupervisorMap.css">
+    <style>
+
+
+    </style>
+</head>
+
+<body class="cbp-spmenu-push">
+	<div class="main-content">
+		<!--left-fixed -navigation-->
+		<?php include_once('includes/sidebar.php'); ?>
+		<!--left-fixed -navigation-->
+		<!-- header-starts -->
+		<?php include_once('includes/header.php'); ?>
+		<!-- //header-ends -->
+		<!-- main content start-->
+		<div id="page-wrapper">
+			<div class="main-page">
+				
+			</div>
+		</div>
+		<footer><?php include_once('includes/footer.php'); ?></footer>
+		<!-- Classie -->
+		<script src="js/classie.js"></script>
+		<script>
+			var menuLeft = document.getElementById('cbp-spmenu-s1'),
+				showLeftPush = document.getElementById('showLeftPush'),
+				body = document.body;
+
+			showLeftPush.onclick = function() {
+				classie.toggle(this, 'active');
+				classie.toggle(body, 'cbp-spmenu-push-toright');
+				classie.toggle(menuLeft, 'cbp-spmenu-open');
+				disableOther('showLeftPush');
+			};
+
+			function disableOther(button) {
+				if (button !== 'showLeftPush') {
+					classie.toggle(showLeftPush, 'disabled');
+				}
+			}
+		</script>
+		<!--scrolling js-->
+		<script src="js/jquery.nicescroll.js"></script>
+		<script src="js/scripts.js"></script>
+		<!--//scrolling js-->
+		<!-- Bootstrap Core JavaScript -->
+		<script src="js/bootstrap.js"> </script>
+</body>
