8c6e76e4ba0edb2681efec16372bb55e29729d59
Wei Kang Yi <weiky-pm19@student.tarc.edu.my>
2022-09-09 21:59:48 +0800

    update

--- Client/view/page/ky-StudentRegister.php
+++ Client/view/page/ky-StudentRegister.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 error_reporting(0);
-include('includes/db_connection.php');
+include('includes/dbconnection.php');
 /*if (strlen($_SESSION['bpmsaid'] == 0)) {
 	header('location:logout.php');
 } else {
@@ -26,35 +26,43 @@ include('includes/db_connection.php');
 <html>
 
 <head>
-    <title>ITP SYSTEM</title>
-    <script type="application/x-javascript">
-        addEventListener("load", function() {
-            setTimeout(hideURLbar, 0);
-        }, false);
+	<title>ITP System | Dashboard</title>
 
-        function hideURLbar() {
-            window.scrollTo(0, 1);
-        }
-    </script>
-    <link href="../../css/bootstrap.css" rel='stylesheet' type='text/css' />
-    <link href="../../css/style.css" rel='stylesheet' type='text/css' />
-    <link href="../../css/font-awesome.css" rel="stylesheet">
-    <script src="../../js/jquery-1.11.1.min.js"></script>
-    <script src="../../js/modernizr.custom.js"></script>
-    <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link href="../../css/animate.css" rel="stylesheet" type="text/css" media="all">
-    <script src="../../js/wow.min.js"></script>
-    <script>
-        new WOW().init();
-    </script>
-    <script src="../../js/metisMenu.min.js"></script>
-    <script src="../../js/custom.js"></script>
-    <link href="../../css/custom.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../scss/studentSupervisorMap.css">
-    <style>
+	<script type="application/x-javascript">
+		addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
 
-
-    </style>
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<!-- Custom CSS -->
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<!-- font CSS -->
+	<!-- font-awesome icons -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- //font-awesome icons -->
+	<!-- js-->
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	<!--webfonts-->
+	<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<!--//webfonts-->
+	<!--animate-->
+	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+	<script src="js/wow.min.js"></script>
+	<script>
+		new WOW().init();
+	</script>
+	<!--//end-animate-->
+	<!-- Metis Menu -->
+	<script src="js/metisMenu.min.js"></script>
+	<script src="js/custom.js"></script>
+	<link href="css/custom.css" rel="stylesheet">
+	<!--//Metis Menu -->
 </head>
 
 <body class="cbp-spmenu-push">
@@ -101,3 +109,5 @@ include('includes/db_connection.php');
 </body>
 
 </html>
+<?php //} 
+?>
