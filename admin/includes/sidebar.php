48ff9dd1e17b50a3630fc49faa170537418106f0
Nei Jun Xian <neijunxian@gmail.com>
2022-12-08 17:56:31 +0800

    update sidebar logout

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -192,7 +192,7 @@ if(isset($_SESSION)){
         </li>
 
         <li>
-          <a href="index.php" class="chart-nav"><i class="fa fa-sign-out nav_icon"></i>Sign Out</a>
+          <a href="../../app/BLL/logoutBLL.php" class="chart-nav"><i class="fa fa-sign-out nav_icon"></i>Sign Out</a>
         </li>
         <hr style="background-color:transparent; border:none;">
         <hr style="background-color:transparent; border:none;">
