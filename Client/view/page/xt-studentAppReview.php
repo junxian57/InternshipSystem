e0392da386c54db4fe8de8a57261e5cb80926429
XiaoTong <wongxt-wm19@student.tarc.edu.my>
2022-12-14 23:37:19 +0800

    Display Student CV

--- Client/view/page/xt-studentAppReview.php
+++ Client/view/page/xt-studentAppReview.php
@@ -47,6 +47,13 @@
 			window.scrollTo(0, 1);
 		}
 	</script>
+
+  <style>
+    #studentCV{
+      width: 890px;
+      height: 1000px;
+    }
+  </style>
 </head>
 
 <body class="cbp-spmenu-push">
@@ -193,13 +200,16 @@
             </div>
 
             <div class="subtitle">
-              <h2 class="sub-3">Supporting Document</h2>
+              <h2 class="sub-3">Supporting Document - Student CV</h2>
             </div>
             
             <div class="inputBox">
               <div class="viewInput">
-                <span>Student CV</span>
-                <input type="file" name="cmpCompanySize" readonly value="<?php echo$cmpCompanySize; ?>">
+                <div id="studentCV">
+                <?php
+                  echo '<iframe src="../../../Client/view/document/StudentCV/'.$studentID.' - '.$studName.'.pdf" width="100%" style="height:100%"></iframe>';
+                ?>
+                </div>
               </div>
             </div>
             
