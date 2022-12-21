e79766b742b8e04413d1d6564bcb354387a53b5f
CheeHua <chiach-wm19@student.tarc.edu.my>
2022-12-21 15:17:59 +0800

    emaildto

--- admin/app/DTO/visitationMapListDTO.php
+++ admin/app/DTO/visitationMapListDTO.php
@@ -6,13 +6,17 @@ class visitationMapListDTO
     private $Visitation_AppMapID;
     private $lecturerID;
     private $lecName;
+
+    private $lecEmail;
+
     private $Visitation_AssignedCmpID;
 
-    public function __construct($Visitation_AppMapID, $lecturerID, $lecName,$Visitation_AssignedCmpID)
+    public function __construct($Visitation_AppMapID, $lecturerID, $lecName,$lecEmail,$Visitation_AssignedCmpID)
     {
         $this->Visitation_AppMapID = $Visitation_AppMapID;
         $this->lecturerID = $lecturerID;
         $this->lecName = $lecName;
+        $this->lecEmail = $lecEmail;
         $this->Visitation_AssignedCmpID = $Visitation_AssignedCmpID;
     }
 
@@ -30,6 +34,13 @@ class visitationMapListDTO
     {
         return $this->lecName;
     }
+
+    
+    public function getlecEmail()
+    {
+        return $this->lecEmail;
+    }
+    
     public function Visitation_AssignedCmpID()
     {
         return $this->Visitation_AssignedCmpID;
